8b2c14b7f9540496019e51821caf2f752dbec91f
pukuba <20sunrin041@sunrint.hs.kr>
2021-01-27 21:57:37 +0900

    Refactoring

--- client/js/api.js
+++ client/js/api.js
@@ -20,46 +20,46 @@ const get_lists = async (address, duration, arrivalRate, clientCount) => {
 }
 
 const api = async () => {
-    document.getElementById("end").style.display = "none";
-    document.getElementById("result").style.display = "none";
+    document.getElementById("end").style.display = "none"
+    document.getElementById("result").style.display = "none"
 
-    const val = document.getElementById("forms");
+    const val = document.getElementById("forms")
 
     if (val[3].value * val[4].value > 1000 || val[2].value > 30 || (val[0].value.substr(0, 8) != "https://" && val[0].value.substr(0, 7) != "http://")) {
-        document.getElementById("error").style.display = "block";
-        document.getElementById("error_").innerHTML = "";
+        document.getElementById("error").style.display = "block"
+        document.getElementById("error_").innerHTML = ""
         if (val[0].value.substr(0, 8) != "https://" && val[0].value.substr(0, 7) != "http://") {
-            document.getElementById("error_").innerHTML += "URL 이 잘못되었습니다.<br>";
+            document.getElementById("error_").innerHTML += "URL 이 잘못되었습니다.<br>"
         }
         if (val[2].value > 30) {
-            document.getElementById("error_").innerHTML += "지속 시간은 30초보다 짧아야합니다.<br>";
+            document.getElementById("error_").innerHTML += "지속 시간은 30초보다 짧아야합니다.<br>"
         }
         if (val[3].value * val[4].value > 1000) {
-            document.getElementById("error_").innerHTML += "접속횟수 * 접속인원은 1000 보다 작야아합니다.";
+            document.getElementById("error_").innerHTML += "접속횟수 * 접속인원은 1000 보다 작야아합니다."
         }
     }
     else {
-        document.getElementById("end").style.display = "block";
+        document.getElementById("end").style.display = "block"
         document.getElementById("load").style.display = "block"
 
         fnMove();
 
-        let all_list = await get_lists(val[0].value, Number(val[2].value), Number(val[3].value), Number(val[4].value));
+        let all_list = await get_lists(val[0].value, Number(val[2].value), Number(val[3].value), Number(val[4].value))
 
         document.getElementById("load").style.display = "none"
-        document.getElementById("result").style.display = "block";
+        document.getElementById("result").style.display = "block"
         //print time
         test_time()
 
         document.getElementById("json").textContent = JSON.stringify(all_list, undefined, 2)
 
         //print Codes
-        let tag = "";
+        let tag = ""
         for (const row in all_list["Codes"]) {
-            if (all_list["Codes"][Number(row)] == undefined) all_list["Codes"][Number(row)] = "Null";
+            if (all_list["Codes"][Number(row)] == undefined) all_list["Codes"][Number(row)] = "Null"
             tag += "<tr><td class='codes'>" + row + "</td><td>" + all_list["Codes"][Number(row)] + "</td></tr>"
         }
-        document.getElementById("code_table").innerHTML = tag;
+        document.getElementById("code_table").innerHTML = tag
         //print Scenario Counts
         document.getElementById("scenario_0").innerHTML = all_list["Scenario counts"][0]
 
